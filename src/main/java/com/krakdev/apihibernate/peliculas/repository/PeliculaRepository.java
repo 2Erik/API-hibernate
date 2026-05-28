2dbd536b3aa12ef5f8b7d4be023623776dd9e73f
2Erik <erik19yogo@gmail.com>
2026-05-28 14:41:07 -0500

    ERT: Interfaz PeliculaRepository

---
+++ src/main/java/com/krakdev/apihibernate/peliculas/repository/PeliculaRepository.java
@@ -0,0 +1,20 @@
+package com.krakdev.apihibernate.peliculas.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.krakdev.apihibernate.peliculas.entidades.Pelicula;
+
+
+@Repository
+public interface PeliculaRepository
+        extends JpaRepository<Pelicula, Long> {
+
+    List<Pelicula> findByGenero(String genero);
+
+    List<Pelicula> findByDisponible(boolean disponible);
+
+}
